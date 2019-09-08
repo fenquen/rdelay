d3031d786387912832b5b8411076c7a3288aec14
fenquen <fenquen@163.com>
2019-09-08 15:37:41 +0800

    client send and receive into seperate modules

--- rdelay-client-send/src/main/java/com/fenquen/rdealy/client/send/TaskSender.java
+++ rdelay-client-send/src/main/java/com/fenquen/rdealy/client/send/TaskSender.java
@@ -1,14 +1,10 @@
-package com.fenquen.rdelay.client.send;
+package com.fenquen.rdealy.client.send;
 
 import com.alibaba.fastjson.JSON;
-import com.fenquen.rdelay.model.annotation.Nullable;
 import com.fenquen.rdelay.model.req.create_task.Req4CreateTask;
 import com.fenquen.rdelay.model.resp.Resp4CreateTask;
-import com.fenquen.rdelay.utils.BeanUtils;
 import com.fenquen.rdelay.utils.HttpUtils;
 import com.fenquen.rdelay.utils.TextUtils;
-
-import java.lang.reflect.Field;
 
 public class TaskSender {
     private static final String DEST_URI = "/createTask";

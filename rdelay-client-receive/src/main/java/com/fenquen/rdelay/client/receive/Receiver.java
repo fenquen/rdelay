87579f512fd53dc187ef349d188f275a5a122d70
fenquen <fenquen@163.com>
2019-09-08 21:14:18 +0800

    preliminary support for reflection receiver

--- rdelay-client-receive/src/main/java/com/fenquen/rdelay/client/receive/Receiver.java
+++ rdelay-client-receive/src/main/java/com/fenquen/rdelay/client/receive/Receiver.java
@@ -1,17 +1,29 @@
 package com.fenquen.rdelay.client.receive;
 
+import com.alibaba.fastjson.JSON;
 import com.fenquen.rdelay.model.task.AbstractTask;
 import com.fenquen.rdelay.model.execution.ExecutionResp;
 import com.fenquen.rdelay.model.task.ReflectionTask;
 import com.fenquen.rdelay.model.task.StrContentTask;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Map;
+
 @RestController
-public class Receiver {
+public class Receiver implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
 
     @Autowired(required = false)
     private TaskConsumer taskConsumer;
@@ -36,6 +48,59 @@ public class Receiver {
     public ExecutionResp receiveReflectTask(@RequestBody ReflectionTask reflectionTask) {
         ExecutionResp timeUpResp = new ExecutionResp();
         try {
+            // paramTypes
+            Class[] paramTypes = new Class[reflectionTask.paramTypeNames.length];
+            int a = 0;
+            for (String paramTypeName : reflectionTask.paramTypeNames) {
+                paramTypes[a++] = Class.forName(paramTypeName);
+            }
+
+            // params
+            Object[] params = new Object[reflectionTask.params.length];
+            for (int b = 0; params.length > b; b++) {
+                params[b] = JSON.parseObject(reflectionTask.params[b], paramTypes[b]);
+            }
+
+            // targetClass
+            Class targetClass = Class.forName(reflectionTask.className);
+
+            // targetMethod
+            Method targetMethod = null;
+            try {
+                targetMethod = targetClass.getDeclaredMethod(reflectionTask.methodName, paramTypes);
+            } catch (NoSuchMethodException e) {
+                // noop
+            }
+
+            if (null == targetMethod) {
+                targetMethod = targetClass.getMethod(reflectionTask.methodName, paramTypes);
+            }
+
+            targetMethod.setAccessible(true);
+
+            // trig method when static
+            if (Modifier.isStatic(targetMethod.getModifiers())) {
+                targetMethod.invoke(paramTypes, params);
+                timeUpResp.success();
+                return timeUpResp;
+            }
+
+            // not static,get the instance
+            Object targetObj = null;
+
+            Map<String, Object> beanMap = applicationContext.getBeansOfType(targetClass, true, false);
+
+            if (beanMap.size() == 0) {
+                if (targetClass.isInterface() || Modifier.isAbstract(targetClass.getModifiers())) {
+                    throw new NoSuchBeanDefinitionException(targetClass);
+                }
+
+                targetObj = targetClass.newInstance();
+            } else {
+                targetObj = beanMap.values().toArray()[0];
+            }
+
+            targetMethod.invoke(targetObj, params);
             timeUpResp.success();
         } catch (Exception e) {
             e.printStackTrace();
@@ -45,4 +110,7 @@ public class Receiver {
         return timeUpResp;
     }
 
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+
+    }
 }

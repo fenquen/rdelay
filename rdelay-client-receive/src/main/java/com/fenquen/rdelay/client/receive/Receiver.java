bc94357990e8dea23800affea49f491ce4789fbb
fenquen <fenquen@163.com>
2019-09-08 19:17:32 +0800

    use different url to receive different kinds of tasks that are timeup

--- rdelay-client-receive/src/main/java/com/fenquen/rdelay/client/receive/Receiver.java
+++ rdelay-client-receive/src/main/java/com/fenquen/rdelay/client/receive/Receiver.java
@@ -2,6 +2,8 @@ package com.fenquen.rdelay.client.receive;
 
 import com.fenquen.rdelay.model.task.AbstractTask;
 import com.fenquen.rdelay.model.execution.ExecutionResp;
+import com.fenquen.rdelay.model.task.ReflectionTask;
+import com.fenquen.rdelay.model.task.StrContentTask;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,13 +16,26 @@ public class Receiver {
     @Autowired(required = false)
     private TaskConsumer taskConsumer;
 
-    @RequestMapping(value = "/rdelay/receiveTask/", method = RequestMethod.POST)
-    public ExecutionResp receive(@RequestBody AbstractTask task) {
+    @RequestMapping(value = "/rdelay/receiveTask/STR_CONTENT", method = RequestMethod.POST)
+    public ExecutionResp receiveStrContentTask(@RequestBody StrContentTask task) {
         ExecutionResp timeUpResp = new ExecutionResp();
         try {
             if (taskConsumer != null) {
                 taskConsumer.consumeTask(task);
             }
+            timeUpResp.success();
+        } catch (Exception e) {
+            e.printStackTrace();
+            timeUpResp.fail(e);
+        }
+
+        return timeUpResp;
+    }
+
+    @RequestMapping(value = "/rdelay/receiveTask/REFLECT", method = RequestMethod.POST)
+    public ExecutionResp receiveReflectTask(@RequestBody ReflectionTask task) {
+        ExecutionResp timeUpResp = new ExecutionResp();
+        try {
             timeUpResp.success();
         } catch (Exception e) {
             e.printStackTrace();

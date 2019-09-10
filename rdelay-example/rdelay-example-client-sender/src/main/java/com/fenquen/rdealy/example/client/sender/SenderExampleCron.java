c1613d8de88c73fce202565ebfcdce1023e59690
fenquen <fenquen@163.com>
2019-09-10 15:12:38 +0800

    add crontask sender example

--- rdelay-example/rdelay-example-client-sender/src/main/java/com/fenquen/rdealy/example/client/sender/SenderExampleCron.java
+++ rdelay-example/rdelay-example-client-sender/src/main/java/com/fenquen/rdealy/example/client/sender/SenderExampleCron.java
@@ -4,17 +4,23 @@ import com.fenquen.rdealy.client.sender.TaskSender;
 import com.fenquen.rdelay.model.req.create_task.Req4CreateStrContentTask;
 import com.fenquen.rdelay.model.resp.Resp4CreateTask;
 
-public class SenderExample {
+public class SenderExampleCron {
     public static void main(String[] args) throws Exception {
         // designate the rdelay server address where the task to be sent
         TaskSender.setDestSvrAddr("http://127.0.0.1:8086");
 
-        //  while (true) {
+
         Req4CreateStrContentTask req4CreateStrContentTask = new Req4CreateStrContentTask();
 
         req4CreateStrContentTask.bizTag = "testBizTag";
-        // run after 2s
+
+        // enable cron
+        req4CreateStrContentTask.enableCron = true;
+        // when cron is enabled,"executionTime" will be ignored
         req4CreateStrContentTask.executionTime = System.currentTimeMillis() + 2000;
+        // cron expression,executed per 2h
+        req4CreateStrContentTask.cronExpression = "0/10 * * * * ? ";
+
         req4CreateStrContentTask.executionAppSvrAddr = "http://127.0.0.1:8080";
         req4CreateStrContentTask.content = "testContent";
 
@@ -22,7 +28,5 @@ public class SenderExample {
 
         System.out.println(resp4CreateTask.success + "_" + resp4CreateTask.errMsg);
 
-        //  Thread.sleep(60000000);
-        //  }
     }
 }
